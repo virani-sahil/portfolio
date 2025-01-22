1f562f769375349949dc3964f0ebe79e563d2683
Sahil <viranisahil004@gmail.com>
2025-01-22 18:04:29 +0530

    manage toggle menu

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
-const Footer = ({ children }) => {
+const Footer = () => {
     return (
         <>
-            {children}
+
             <div className="bg-black p-2 flex justify-between items-center w-[100%] px-[10%]">
                 <div className='text-white'>
                     <h1 >- Developed by</h1>
                     <h1 className="font-bold text-2xl"> SAHILBHAI VIRANI</h1>
                 </div>
-                <a href="#home"><button className="bg-mainColor text-black font-bold p-1 rounded-md">Home</button></a>
+                <a href="#home"><button className="bg-mainColor text-black font-bold py-1 px-2 rounded-md">Home</button></a>
             </div>
         </>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import { Link, NavLink } from "react-router"
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    
+
     return (
-        <div  className="top-0 bg-mainBg left-0 w-[100%] text-white flex justify-between sticky z-10 items-center py-7 font-serif font-extrabold px-[10%]">
+        <div className="top-0 bg-mainBg left-0 w-[100%] text-white flex justify-between sticky z-10 items-center py-7 font-serif font-extrabold px-[10%]">
             <div>
                 <Link to="/" className="max-[425px]:text-3xl text-5xl"><span className="hover:text-mainColor">Port</span><span className="hover:text-mainColor">folio</span></Link>
             </div>
@@ -17,12 +17,12 @@ const Header = () => {
                 <NavLink to="/project">Project</NavLink>
                 <NavLink to="/contact">Contact</NavLink>
             </div>
-            <button className="header-custom:hidden min-[425px]:text-2xl hover:underline" onClick={() => { setIsMenuOpen(!isMenuOpen) }}><span className="hover:text-[#0ef]">M</span><span className="hover:text-[#0ef]">E</span><span className="hover:text-[#0ef]">N</span><span className="hover:text-[#0ef]">U</span></button>
+            <button className="header-custom:hidden min-[425px]:text-2xl hover:underline" onClick={() => (setIsMenuOpen(!isMenuOpen))}><span className="hover:text-[#0ef]">M</span><span className="hover:text-[#0ef]">E</span><span className="hover:text-[#0ef]">N</span><span className="hover:text-[#0ef]">U</span></button>
 
             {/* Mobile Menu */}
             {
                 isMenuOpen && (
-                    <div className="shadow-lg shadow-black z-10 grid gap-y-5 left-0 w-full text-center justify-center top-24 bg-white text-black absolute header-custom:gap-28 min-[425px]:text-2xl header-custom:hidden">
+                    <div className="shadow-lg shadow-black z-10 grid gap-y-5 left-0 w-full text-center justify-center top-24 bg-white text-black absolute header-custom:gap-28 min-[425px]:text-2xl header-custom:hidden" onClick={() => (setIsMenuOpen(false))}>
                         <NavLink to="/" className="hover:underline">Home</NavLink>
                         <NavLink to="/about" className="hover:underline">About</NavLink>
                         <NavLink to="/skills" className="hover:underline">Skills</NavLink>
